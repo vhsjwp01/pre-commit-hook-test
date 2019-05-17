8a7dee72a62cd4065a5e38ade0c84a5dec29e92c
Plummer, Jason <jason.plummer@vitalsource.com>
2019-05-17 15:31:12 -0500

    test

--- foop/test.tf
+++ foop/test.tf
@@ -1,6 +1,6 @@
 resource "google_compute_address" "mul-dbv-d01" {
-  name   = "${var.project}-mul-dbv-d01-${local.zone_abv_001}"
-  region = "${local.def_region_001}"
+name   = "${var.project}-mul-dbv-d01-${local.zone_abv_001}"
+region = "${local.def_region_001}"
 
 #
 }
